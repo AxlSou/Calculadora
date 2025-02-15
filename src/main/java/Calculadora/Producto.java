93ca686b17c7dbdf7bbb8d5cdea8389fde6a5fb4
Jorge Vizuete Mendez <jorge.vizuete643@comunidadunir.net>
2025-02-15 17:09:48 +0100

    nueva correccion en Menu

--- src/main/java/Calculadora/Producto.java
+++ src/main/java/Calculadora/Producto.java
@@ -5,27 +5,67 @@ import java.util.Scanner;
 public class Producto {
 
     private static Scanner leer = new Scanner(System.in);
+
     /**
      * Método que imprime el menú por la pantalla
+     *
      * @param opcion (scanner que se introduce en la consola)
      * @return Elige la operación que se desea realizar
      */
     public static int menu() {
+        final Scanner leer = new Scanner(System.in);
         int opcion = 0;
 
-        System.out.println("1.- Producto de dos números reales");
-        System.out.println("2.- Producto de dos números enteros");
-        System.out.println("3.- Productos de tres números reales");
-        System.out.println("4.- Potencia de un número");
-        System.out.println("5.- Salir");
-        System.out.println("Teclea una opción (5 para salir)");
-        opcion = leer.nextInt();
-        while (opcion < 1 || opcion > 5) {
-            System.out.println("Opccion incorrecta. Elige un numero entre 1 y 5.");
+        do {
+            System.out.println("1.- Producto de dos números reales");
+            System.out.println("2.- Producto de dos números enteros");
+            System.out.println("3.- Productos de tres números reales");
+            System.out.println("4.- Potencia de un número");
+            System.out.println("5.- Salir");
+            System.out.println("Teclea una opción (5 para salir)");
             opcion = leer.nextInt();
-        }
 
-        return opcion;
+            opcion = leer.next();
+            switch (opcion) {
+                case 1:
+                    double a;
+                    System.out.println("Introduzca un número real");
+                    a = Producto.verificarReal(leer);
+                    double b;
+                    System.out.println("Introduzca otro número real");
+                    b = Producto.verificarReal(leer);
+                    System.out.println("Resultado = " + Producto.productoReales2(a, b));
+                    break;
+                case 2:
+                    System.out.println("Introduzca un número entero");
+                    int c = Producto.verificarEntero(leer);
+                    System.out.println("Introduzca otro número entero");
+                    int d = Producto.verificarEntero(leer);
+                    System.out.println("Resultado = " + Producto.productoEnteros(c, d));
+                    break;
+                case 3:
+                    System.out.println("Introduzca un número real");
+                    double e = Producto.verificarReal(leer);
+                    System.out.println("Introduzca otro número real");
+                    double f = Producto.verificarReal(leer);
+                    System.out.println("Introduzca otro número real");
+                    double g = Producto.verificarReal(leer);
+                    System.out.println("Resultado = " + Producto.productoReales3(e, f, g));
+                    break;
+                case 4:
+                    System.out.println("Introduzca la base");
+                    double base = Producto.verificarReal(leer);
+                    System.out.println("Introduzca el exponente");
+                    double exp = Producto.verificarReal(leer);
+                    System.out.println("Resultado = " + Producto.potencia(base, exp));
+                    break;
+            }
+
+        } while (opcion != 5);
+
+        System.out.println("FIN DE PROGRAMA");
+
+        leer.close();
     }
 
     /**
@@ -101,52 +141,4 @@ public class Producto {
      * Método para obtener los resultados utilizando los demas métodos
      * @param args
      */
-    public static void main(String[] args) {
-
-        final Scanner leer = new Scanner(System.in);
-
-        int opcion = 0;
-        do {
-            opcion = Producto.menu();
-            switch (opcion) {
-                case 1:
-                    double a;
-                    System.out.println("Introduzca un número real");
-                    a = Producto.verificarReal(leer);
-                    double b;
-                    System.out.println("Introduzca otro número real");
-                    b = Producto.verificarReal(leer);
-                    System.out.println("Resultado = " + Producto.productoReales2(a, b));
-                    break;
-                case 2:
-                    System.out.println("Introduzca un número entero");
-                    int c = Producto.verificarEntero(leer);
-                    System.out.println("Introduzca otro número entero");
-                    int d = Producto.verificarEntero(leer);
-                    System.out.println("Resultado = " + Producto.productoEnteros(c, d));
-                    break;
-                case 3:
-                    System.out.println("Introduzca un número real");
-                    double e = Producto.verificarReal(leer);
-                    System.out.println("Introduzca otro número real");
-                    double f = Producto.verificarReal(leer);
-                    System.out.println("Introduzca otro número real");
-                    double g = Producto.verificarReal(leer);
-                    System.out.println("Resultado = " + Producto.productoReales3(e, f, g));
-                    break;
-                case 4:
-                    System.out.println("Introduzca la base");
-                    double base = Producto.verificarReal(leer);
-                    System.out.println("Introduzca el exponente");
-                    double exp = Producto.verificarReal(leer);
-                    System.out.println("Resultado = " + Producto.potencia(base, exp));
-                    break;
-            }
-
-        } while (opcion != 5);
-
-        System.out.println("FIN DE PROGRAMA");
-
-        leer.close();
-    }
 }

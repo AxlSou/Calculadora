355c59d40f1c0eefa9b5fc2a11c3cde22bda8524
Jorge Vizuete Mendez <jorge.vizuete643@comunidadunir.net>
2025-02-20 19:39:09 +0100

    Test de Trigonomeria

---
+++ src/test/java/org/example/TrigonometriaTest.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class TrigonometriaTest {
+
+    private Trigonometria trigonometria;
+
+    @BeforeEach
+    void setUp() {
+        trigonometria = new Trigonometria();
+    }
+
+    @Test
+    void seno() {
+
+    }
+
+    @Test
+    void coseno() {
+    }
+
+    @Test
+    void tangente() {
+    }
+
+    @Test
+    void menu() {
+    }
+}
